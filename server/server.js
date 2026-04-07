72e5f865b23440ce095589bb6237a97ffb210247
Ahmed-C0der <ahmed55programer@gmail.com>
2026-04-07 22:10:34 +0200

    change alternative

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ Note: Many developers still keep CORS enabled on the backend as a "second layer"
 
 */
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "https://fullstack-lms-platform-six.vercel.app",
     credentials: true
 }))
 
